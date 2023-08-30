1223a8d8077df86e7c93c12376054d3f41c0ca0a
Vanit Yadav <110538930+vanityadav@users.noreply.github.com>
2023-08-30 23:06:51 +0530

    Update CurrentTrack.tsx

--- src/components/spotify-player/CurrentTrack.tsx
+++ src/components/spotify-player/CurrentTrack.tsx
@@ -1,19 +1,22 @@
+import Image from "next/image";
+
 type Props = { track: Spotify.Track | undefined };
 
 export default function CurrentTrack({ track }: Props) {
-  return (
-    <div className="main-wrapper">
-      <img
-        src={track?.album.images[0].url}
-        className="now-playing__cover"
-        alt=""
-      />
+  if (track)
+    return (
+      <div className="main-wrapper">
+        <Image
+          src={track?.album.images[2].url}
+          className="now-playing__cover"
+          alt=""
+        />
 
-      <div className="now-playing__side">
-        <div className="now-playing__name">{track?.name}</div>
+        <div className="now-playing__side">
+          <div className="now-playing__name">{track?.name}</div>
 
-        <div className="now-playing__artist">{track?.artists[0]?.name}</div>
+          <div className="now-playing__artist">{track?.artists[0]?.name}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
